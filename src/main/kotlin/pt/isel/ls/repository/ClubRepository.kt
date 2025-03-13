3cb3c1dbe9f7fcd528a9ad706c33eca6478de927
LeonelCorreia <A50552@alunos.isel.pt>
2025-03-13 15:36:48 +0000

    Added Repository interfaces.

---
+++ src/main/kotlin/pt/isel/ls/repository/ClubRepository.kt
@@ -0,0 +1,18 @@
+package pt.isel.ls.repository
+
+import pt.isel.ls.domain.Club
+
+/**
+ * Generic Interface for a Club repository that supports CRUD operations.
+ */
+interface ClubRepository : Repository<Club> {
+    /**
+     * Function that creates a new Club.
+     * Returns the created element.
+     * cid is automatically incremented so it's not received as a parameter to the function.
+     */
+    fun createClub(
+        name: String,
+        ownerId: Int,
+    ): Club
+}
---
+++ src/main/kotlin/pt/isel/ls/repository/CourtRepository.kt
@@ -0,0 +1,18 @@
+package pt.isel.ls.repository
+
+import pt.isel.ls.domain.Court
+
+/**
+ * Generic Interface for a Court repository that supports CRUD operations.
+ */
+interface CourtRepository : Repository<Court> {
+    /**
+     * Function that creates a new Court.
+     * Returns the created element.
+     * crid is automatically incremented so it's not received as a parameter to the function.
+     */
+    fun createCourt(
+        name: String,
+        clubId: Int,
+    ): Court
+}
